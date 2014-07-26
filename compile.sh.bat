287ec64a68846e6c0e79310a20741af56198ae6b
Michael Gibson <mikar_gibbros@yahoo.com>
2014-07-26 10:15:37 -0600

    updated shell scripts

--- compile.sh.bat
+++ compile.sh.bat
@@ -4,13 +4,13 @@ echo ^>/dev/null \>nul \& goto bat
 
 rem= # .sh mode #
 echo @ command is only used in .bat mode.
-cd engine
-../godot -export "Linux X11" "../alpha"
+cd app
+godot -export "Linux X11" "../alpha"
 exit
 
 rem= # .bat mode #
 :bat
 echo #! command is only used in .sh mode.
-cd engine
-"../godot.exe" -export "Windows Desktop" "../alpha.exe"
+cd app
+"godot.exe" -export "Windows Desktop" "../alpha.exe"
 exit /b
--- edit.sh.bat
+++ edit.sh.bat
@@ -4,13 +4,13 @@ echo ^>/dev/null \>nul \& goto bat
 
 rem= # .sh mode #
 echo @ command is only used in .bat mode.
-cd engine
-../godot -e
+cd app
+godot -e
 exit
 
 rem= # .bat mode #
 :bat
 echo #! command is only used in .sh mode.
-cd engine
-"../godot.exe" -e
+cd app
+"godot.exe" -e
 exit /b
