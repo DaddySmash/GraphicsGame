b2bd3299fdfcc94a7d61d5d3ed1e53b99c7d58d7
Michael Gibson <mikar_gibbros@yahoo.com>
2014-07-26 11:08:15 -0600

    updated shell scripts

--- compile.sh.bat
+++ compile.sh.bat
@@ -5,12 +5,23 @@ echo ^>/dev/null \>nul \& goto bat
 rem= # .sh mode #
 echo @ command is only used in .bat mode.
 cd app
-godot -export "Linux X11" "../alpha"
+if [ ! -x godot ]; then
+  echo&echo Please download, rename, and chmod the Godot Engine to: ./app/godot
+else
+  godot -export "Linux X11" "../alpha"
+fi
+cd ..
 exit
 
 rem= # .bat mode #
 :bat
 echo #! command is only used in .sh mode.
 cd app
-"godot.exe" -export "Windows Desktop" "../alpha.exe"
+if not exist "godot.exe" (
+  echo.&echo Please download and rename the Godot Engine to: ./app/godot.exe
+) else (
+  "godot.exe" -export "Windows Desktop" "../alpha.exe"
+)
+cd ..
+echo.&echo Press any key to close...&pause >nul
 exit /b
