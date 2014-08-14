12c99592daebb25eaefe5674040f5a2af4e244b6
Michael Gibson <mikar_gibbros@yahoo.com>
2014-08-13 20:57:17 -0600

    Working on Menu still.

--- app/code/menu.gd
+++ app/code/menu.gd
@@ -1,21 +1,26 @@
-extends Control
+extends HBoxContainer
 
 func _ready():
 	pass
 
 func _draw():
-	VS.canvas_item_set_clip(get_canvas_item(), true)
+	#VS.canvas_item_set_clip(get_canvas_item(), true)
+	pass
 
-func _on_menu_button_selected( button ):
-	if button == 0:
-		get_node("/root/global").exitGame()
-	if button == 1:
-		get_node("/root/global").start_round(1)
-	if button == 2:
-		get_node("/root/global").start_round(2)
-	if button == 3:
-		get_node("/root/global").start_round(3)
-	if button == 4:
-		get_node("/root/global").start_round(4)
-	if button == 5:
-		pass
+func _on_flee_pressed():
+	get_node("/root/global").exitGame()
+
+func _on_1_pressed():
+	get_node("/root/global").start_round(1)
+
+func _on_2_pressed():
+	get_node("/root/global").start_round(2)
+
+func _on_3_pressed():
+	get_node("/root/global").start_round(3)
+
+func _on_4_pressed():
+	get_node("/root/global").start_round(4)
+
+func _on_donate_pressed():
+	pass
---
+++ app/code/menuItem.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+func _ready():
+	 set_process_input(true)
+
+
+
+
+func _on_Flee_pressed():
+	pass # replace with function body
+
+
+func _on_flee_pressed():
+	pass # replace with function body
