0469ac4e98f6b74e1cc465ddcf2ca88b0037ae96
DaddySmash <DaddySmash@users.noreply.github.com>
2015-05-25 17:59:39 -0600

    Working on calling damage functions

--- app/code/hatHeadThrown.gd
+++ app/code/hatHeadThrown.gd
@@ -7,6 +7,22 @@ var index = null
 func getDamaged():
 	index = get_parent().get_parent().get_parent().getIndexFromNode(self.get_parent())
 	print("Get Damaged ", index)
+	if (get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "empty"):
+		get_parent().get_parent().get_parent().isDied()
+		
+	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "held"):
+		get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").play("breaking")
+		
+	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "swinging"):
+		get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").play("breaking")
+		
+	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "breaking"): #If you are breaking, then you are immune to damage.
+		pass
+		
+	else:
+		print("Death due to error.")
+		get_parent().get_parent().get_parent().isDied()
+		
 	
 func stopMyAnimation(): #Stop the animation and restart the zombie time.
 	index = get_parent().get_parent().get_parent().getIndexFromNode(self.get_parent())
