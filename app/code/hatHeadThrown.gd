983744a3329d54e50f5d77808568f9abe92eda61
DaddySmash <DaddySmash@users.noreply.github.com>
2015-05-25 18:26:55 -0600

    Damage transitions work.

--- app/code/hatHeadThrown.gd
+++ app/code/hatHeadThrown.gd
@@ -1,23 +1,26 @@
 
 #extends AnimatedSprite
 extends TextureButton
-
+#animations
 var index = null
 
 func getDamaged():
 	index = get_parent().get_parent().get_parent().getIndexFromNode(self.get_parent())
 	print("Get Damaged ", index)
-	if (get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "empty"):
+	if (get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").get_current_animation() == "empty"):
+		print("Death due to zombie.")
 		get_parent().get_parent().get_parent().isDied()
 		
-	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "held"):
-		get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").play("breaking")
+	elif (get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").get_current_animation() == "held"):
+		get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").play("breaking")
+		print("Tomb was held.")
 		
-	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "swinging"):
-		get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").play("breaking")
+	elif (get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").get_current_animation() == "swinging"):
+		get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").play("breaking")
+		print("Tomb was swinging.")
 		
-	elif (get_parent().get_parent().get_parent().get_parent().get_node("mouse").get_node("anim").get_current_animation() == "breaking"): #If you are breaking, then you are immune to damage.
-		pass
+	elif (get_parent().get_parent().get_parent().get_node("mouse").get_node("animations").get_node("anim").get_current_animation() == "breaking"): #If you are breaking, then you are immune to damage.
+		print("Tomb was breaking.")
 		
 	else:
 		print("Death due to error.")
